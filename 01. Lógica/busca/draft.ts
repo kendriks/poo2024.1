deaff76f51576e14932494f7818e63aba4c07c0c
kendriks <156589321+kendriks@users.noreply.github.com>
2024-05-08 11:37:08 +0000

    busca

--- 01. Lógica/busca/draft.ts
+++ 01. Lógica/busca/draft.ts
@@ -1,26 +1,98 @@
-
 function inside(vet: number[], value: number): boolean {
+    //IMPLEMENTAÇÃO
+    // for (let i = 0; i < vet.length; i++) {
+    //     if (value === vet[i]) {
+    //         return true;
+    //     }         
+    // }
+    // return false;
+
+    //SIMPLIFICAÇÃO
+    for (let elem of vet) {
+        if (elem === value) {
+        return vet.includes(value);
+        }
+    }
+    return false;
 }
 
 function index_of(vet: number[], value: number): number {
+    //IMPLEMENTAÇÃO
+    // for (let i = 0; i < vet.length; i++) {
+    //     if (value === vet[i]) {
+    //         return i;
+    //     }         
+    // }
+    // return -1;
+
+    //SIMPLIFICAÇÃO
+    for (let i = 0; i < vet.length; i++) {
+        return vet.indexOf(value);
+    }
+
 }
 
 function find_if(vet: number[]): number {
+    //IMPLEMENTAÇÃO
+    // for (let i = 0; i < vet.length; i++) {
+    //     if (vet[i] > 0) {
+    //         return i;
+    //     }         
+    // }
+    // return -1;
+
+    //SIMPLIFICAÇÃO
+    for (let i = 0; i < vet.length; i++) {
+        return vet.findIndex(value => value > 0);
+    }
+
 }
 
 function min_element(vet: number[]): number {
+    //IMPLEMENTAÇÃO
+    // if (vet.length === 0) {
+    //     return -1;
+    // }
+    // let posicion = 1;
+    // for (let i = 0; i < vet.length; i++) {
+    //     if (vet[i] < vet[posicion]) {
+    //         posicion = i;
+    //     } 
+    // }
+    // return posicion;
+
+    //SIMPLIFICAÇÃO
+    let smaller = Math.min(...vet);
+    let position = vet.indexOf(smaller);
+    return position;
 }
 
-function find_min_if(vet: number[]): number {
-}
+function find_min_if(vet: number[]): number { 
+    //IMPLEMENTAÇÃO
+    // let minPosition = -1;
+    // for (let i = 0; i < vet.length; i++) {
+    //     if (vet[i] > 0  && (minPosition === -1 || vet[i] < vet[minPosition])) {
+    //          minPosition = i;
+    //     }
+    // }
+    // return minPosition;
 
+    //SIMPLIFICAÇÃO
+    let positive_numbers = vet.filter(valor => valor > 0);
+    if (positive_numbers.length > 0) {
+        let smaller = Math.min(...positive_numbers);
+        return vet.indexOf(smaller);
+    } else {
+        return -1;
+    }
+}
 
 
 // -------------------------- MAIN --------------------------
 
-let _cin_ : string[] = [];
-try { _cin_ = require("fs").readFileSync(0).toString().split(/\r?\n/); } catch(e){}
-let input = () : string => _cin_.length === 0 ? "" : _cin_.shift()!;
+let cin : string[] = [];
+try { cin = require("fs").readFileSync(0).toString().split(/\r?\n/); } catch(e){}
+let input = () : string => cin.length === 0 ? "" : cin.shift()!;
 let write = (text: any, end:string="\n")=> process.stdout.write("" + text + end);
 
 function main() {
@@ -69,5 +141,3 @@ function to_vet(token: string): number[] {
     let inside = token.substring(1, size - 1);
     return inside === "" ? [] : inside.split(",").map(x => +x)
 }
-
-
